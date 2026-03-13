4b4f9bf73c495958dba3b5759fda0a58970ae6d1
Gonzalo Casas <casas@arch.ethz.ch>
2026-03-13 23:41:48 +0100

    Update inits at top level

--- src/compas_xr/mqtt/__init__.py
+++ src/compas_xr/mqtt/__init__.py
@@ -1,26 +1,3 @@
-"""
-********************************************************************************
-compas_xr.mqtt
-********************************************************************************
-
-This package contains classes for interfacing with MQTT protocol.
-
-.. currentmodule:: compas_xr.mqtt
-
-MQTT Messages
--------------
-
-.. autosummary::
-    :toctree: generated/
-    :nosignatures:
-
-    GetTrajectoryRequest
-    GetTrajectoryResult
-    ApproveTrajectory
-    SendTrajectory
-
-"""
-
 from .messages import ApproveTrajectory, GetTrajectoryRequest, GetTrajectoryResult, SendTrajectory
 
 __all__ = ["GetTrajectoryRequest", "GetTrajectoryResult", "ApproveTrajectory", "SendTrajectory"]
--- src/compas_xr/realtime_database/__init__.py
+++ src/compas_xr/realtime_database/__init__.py
@@ -1,23 +1,3 @@
-"""
-********************************************************************************
-compas_xr.realtime_database
-********************************************************************************
-
-This package contains classes for using Firebase realtime database.
-
-.. currentmodule:: compas_xr.realtime_database
-
-Classes
--------
-
-.. autosummary::
-    :toctree: generated/
-    :nosignatures:
-
-    RealtimeDatabase
-
-"""
-
 from compas_xr.realtime_database.realtime_database import RealtimeDatabase
 
 __all__ = ["RealtimeDatabase"]
